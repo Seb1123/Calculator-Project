6b95df68a92ff8da276f891b7d397c308a5f88f0
Seb1123 <sebastianierquiaga@gmail.com>
2023-10-05 22:00:33 -0400

    Added more functionality to the Calculator project

--- calculator.js
+++ calculator.js
@@ -17,6 +17,7 @@ const divide = function(num1,num2) {
 let var1
 let oper
 let var2
+let expr = [] // The whole mathematical expression: (number), (operation), (number)
 
 const operate = function(op,var1,var2) {
     if (op == 'add') {
@@ -29,3 +30,10 @@ const operate = function(op,var1,var2) {
         return divide(var1,var2)
     }
 }
+
+const display = document.querySelector('.display')
+const digit = document.querySelector('.digit');
+digit.addEventListener('click', () => {
+    expr.push(digit);
+    display.textContent = `${expr.toString()}`;
+});
