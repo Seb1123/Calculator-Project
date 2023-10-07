2eb58518f66d7d92f07213071549d37bc1ad3bc7
Seb1123 <sebastianierquiaga@gmail.com>
2023-10-07 15:54:56 -0400

    Added more stuff.

--- calculator.js
+++ calculator.js
@@ -14,11 +14,6 @@ const divide = function(num1,num2) {
     return num1 / num2
 }
 
-let var1
-let oper
-let var2
-let expr = [] // The whole mathematical expression: (number), (operation), (number)
-
 const operate = function(op,var1,var2) {
     if (op == 'add') {
         return add(var1,var2)
@@ -31,9 +26,41 @@ const operate = function(op,var1,var2) {
     }
 }
 
+let exprBig = []        // The whole mathematical expression: (number), (operation), (number).
+let exprSmall = []      // Stores the current active number/expression that is being typed into the calculator.
+let num = 0
 const display = document.querySelector('.display')
-const digit = document.querySelector('.digit');
-digit.addEventListener('click', () => {
-    expr.push(digit);
-    display.textContent = `${expr.toString()}`;
+const digit = document.querySelectorAll('button.digit')
+const btn = document.querySelectorAll('button')
+
+btn.forEach((button) => {
+    if (button.getAttribute('class') == 'digit'){
+        button.addEventListener('click', () => {
+            exprSmall.push(button.id);
+            num = 0
+            for(i = 0; i <= (exprSmall.length-1); i++) {
+                num += (exprSmall[i]*(10**(exprSmall.length - 1 - i)))
+            }
+            display.textContent = `${num}`;
+        });
+    } else if (button.getAttribute('class') == 'operation') {
+        button.addEventListener('click', (button) => {
+        exprBig.push(num);
+        if (button.getAttribute('id') == 'add') {
+            op = 'add';
+            alert('Hello');
+        };
+        num = 0;
+        });
+    } else if (button.getAttribute('class') == 'neutral') {
+        button.addEventListener('click', () => {
+            exprBig = [];
+            exprSmall = [];
+            num = 0;
+            display.textContent = `${num}`
+        });
+    }
+    ;
 });
+display.textContent = '5318008'; // Displays the default before pressing any numbers.
+
